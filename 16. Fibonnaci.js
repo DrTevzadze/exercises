daf40ac50dacd321dd8ffed83aba1fe4904f9f63
DrTevzadze <dr.davidtevzadze@gmail.com>
2025-03-18 14:58:42 +0400

    Add for loop method

--- 16. Fibonnaci.js
+++ 16. Fibonnaci.js
@@ -3,6 +3,7 @@
 const number = 6;
 
 // Recursive method F(n) = F(n - 1) + F(n - 2);
+// O(2^n)
 const fibonnaci = (num) => {
   if (num === 1) return 0;
   if (num === 2) return 1;
@@ -10,4 +11,18 @@ const fibonnaci = (num) => {
   return fibonnaci(num - 1) + fibonnaci(num - 2);
 };
 
+// Iterative method
+// O(n)
+const fibonnaciLoop = (num) => {
+  const fibos = [0, 1];
+  const iterations = num - fibos.length;
+
+  for (let i = 0; i < iterations; i++) {
+    let nextFiboNumb = fibos[i] + fibos[i + 1];
+    fibos.push(nextFiboNumb);
+  }
+  return fibos[fibos.length - 1];
+};
+
 console.log(fibonnaci(number)); // 5
+console.log(fibonnaciLoop(number)); // 5
