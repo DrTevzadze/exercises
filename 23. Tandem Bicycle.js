a36562e6ba2bb2755860e151d90003b8f25f5437
DrTevzadze <dr.davidtevzadze@gmail.com>
2025-03-20 14:55:26 +0400

    23. Tandem Bicycle - Solved

---
+++ 23. Tandem Bicycle.js
@@ -0,0 +1,16 @@
+const redShirtSpeeds = [5, 5, 3, 9, 2];
+const blueShirtSpeeds = [3, 6, 7, 2, 1];
+
+const tandem = (red, blue) => {
+  const sortRed = red.sort((a, b) => a - b);
+  const sortBlue = blue.sort((a, b) => b - a); //DESC order;
+
+  let totalSum = 0;
+
+  for (let i = 0; i < sortRed.length; i++) {
+    totalSum += Math.max(sortRed[i], sortBlue[i]);
+  }
+  return totalSum;
+};
+
+console.log(tandem(redShirtSpeeds, blueShirtSpeeds)); // 25 is wrong - should be 32.
